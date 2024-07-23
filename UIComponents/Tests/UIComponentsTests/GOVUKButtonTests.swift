5e6ac3909418a4c9d6649b46151fe038841174e4
Robin Phillips <rlphillips@deloitte.co.uk>
2024-07-23 11:54:46 +0100

    fixing current tests

--- UIComponents/Tests/UIComponentsTests/GOVUKButtonTests.swift
+++ UIComponents/Tests/UIComponentsTests/GOVUKButtonTests.swift
@@ -1,28 +1,10 @@
 @testable import UIComponents
 import XCTest
 
-struct MockViewModel: ButtonViewModel {
-    var localisedTitle: String = "configured button"
-
-    var action: () async throws -> Void
-
-    var buttonConfiguration: GOVUKButton.ButtonConfiguration? = .init(
-        titleColorNormal: .purple,
-        titleColorFocused: .green,
-        titleFont: .title3,
-        backgroundColorNormal: .green,
-        backgroundColorFocused: .cyan,
-        buttonShape: .roundedRect(5))
-
-    init(action: @escaping () -> Void) {
-        self.action = action
-    }
-}
-
 final class GOVUKButtonTests: XCTestCase {
     var sut: GOVUKButton!
     var didTap: Bool!
-    var viewModel: ButtonViewModel!
+    var viewModel: GOVUKButton.ButtonViewModel!
     let buttonShapesEnabled = UIAccessibility.buttonShapesEnabled
 
     override func setUp() {
@@ -31,7 +13,7 @@ final class GOVUKButtonTests: XCTestCase {
         sut = GOVUKButton(.secondary)
         didTap = false
 
-        viewModel = MockViewModel {
+        viewModel = GOVUKButton.ButtonViewModel(localisedTitle: "configured button"){
             print("configured button was tapped")
             self.didTap = true
         }
@@ -50,54 +32,28 @@ final class GOVUKButtonTests: XCTestCase {
 extension GOVUKButtonTests {
     func test_unconfiguredButton() {
         XCTAssertNotNil(sut)
-        
-        XCTAssertNil(sut.backgroundColor)
-        XCTAssertFalse(sut.backgroundManager.hasBackground)
-
+        XCTAssertEqual(sut.backgroundColor, UIColor.clear)
         XCTAssertNil(sut.titleLabel?.text)
     }
 
     func test_basicButton() {
         XCTAssertNotNil(sut)
-
         sut.titleLabel?.text = "test button"
-
         XCTAssertEqual(sut.titleLabel?.text, "test button")
-        XCTAssertEqual(sut.titleLabel?.textColor, UIColor.label)
+        XCTAssertEqual(sut.titleLabel?.textColor,
+                       UIColor(resource: UIComponents.ColorResource.accent))
     }
 
     func test_createdFromButtonExtension() {
         sut = GOVUKButton(.secondary)
-
-        XCTAssertNil(sut.backgroundColor)
-        XCTAssertFalse(sut.backgroundManager.hasBackground)
+        XCTAssertEqual(sut.backgroundColor, UIColor.clear)
     }
 
     func test_buttonBackground() {
-        XCTAssertNil(sut.backgroundColor)
-        XCTAssertFalse(sut.backgroundManager.hasBackground)
-
+        XCTAssertEqual(sut.backgroundColor, UIColor.clear)
         sut.backgroundColor = .cyan
-
-        XCTAssertTrue(sut.backgroundManager.hasBackground)
         XCTAssertNotNil(sut.backgroundColor)
-
         sut.backgroundColor = .clear
-
-        XCTAssertFalse(sut.backgroundManager.hasBackground)
         XCTAssertNotNil(sut.backgroundColor)
     }
-
-    func test_addBackground() {
-        let sut = sut.addBackgroundTo()
-        sut.setTitle("test title", for: .normal)
-
-        XCTAssertEqual(sut.layer.cornerRadius, 22)
-    }
-
-//    func test_buttonViewModelConfiguration() {
-//        sut = UIButton.govUK.primary
-//        sut.viewModelUpdate(viewModel: viewModel)
-//
-//    }
 }
