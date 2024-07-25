7defb2199bce4bbd664ccedf296a0511d1678a2b
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-07-25 15:46:51 +0100

    Used color literals over RGB

--- UIComponents/Sources/Colors/UIColor+Convenience.swift
+++ UIComponents/Sources/Colors/UIColor+Convenience.swift
@@ -2,121 +2,28 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static let blue1: UIColor = .init(
-        r: 29,
-        g: 112,
-        b: 184
-    )
+    static let blue1: UIColor = #colorLiteral(red: 0.1137254902, green: 0.4392156863, blue: 0.7215686275, alpha: 1)
+    static let blue2: UIColor = #colorLiteral(red: 0.1450980392, green: 0.6039215686, blue: 1, alpha: 1)
+    static let blue3: UIColor = #colorLiteral(red: 0, green: 0.8196078431, blue: 1, alpha: 1)
+    static let blue4: UIColor = #colorLiteral(red: 0.2705882353, green: 0.9490196078, blue: 0.9921568627, alpha: 1)
+    static let blue5: UIColor = #colorLiteral(red: 0, green: 0.1882352941, blue: 0.4705882353, alpha: 1)
+    static let blue6: UIColor = #colorLiteral(red: 0.4784313725, green: 0.7607843137, blue: 1, alpha: 1)
 
-    static let blue2: UIColor = .init(
-        r: 37,
-        g: 154,
-        b: 255
-    )
+    static let yellow: UIColor = #colorLiteral(red: 1, green: 0.8666666667, blue: 0, alpha: 1)
 
-    static let blue3: UIColor = .init(
-        r: 0,
-        g: 209,
-        b: 255
-    )
-
-    static let blue4: UIColor = .init(
-        r: 69,
-        g: 242,
-        b: 253
-    )
-
-    static let blue5: UIColor = .init(
-        r: 0,
-        g: 48,
-        b: 120
-    )
-
-    static let blue6: UIColor = .init(
-        r: 122,
-        g: 194,
-        b: 255
-    )
-
-    static let yellow: UIColor = .init(
-        r: 255,
-        g: 221,
-        b: 0
-    )
-
-    static let grey900: UIColor = .init(
-        r: 26,
-        g: 26,
-        b: 26
-    )
-
-    static let grey800: UIColor = .init(
-        r: 51,
-        g: 51,
-        b: 51
-    )
-
-    static let grey700: UIColor = .init(
-        r: 77,
-        g: 77,
-        b: 77
-    )
-
-    static let grey600: UIColor = .init(
-        r: 102,
-        g: 102,
-        b: 102
-    )
-
-    static let grey500: UIColor = .init(
-        r: 128,
-        g: 128,
-        b: 128
-    )
-
-    static let grey400: UIColor = .init(
-        r: 153,
-        g: 153,
-        b: 153
-    )
-
-    static let grey300: UIColor = .init(
-        r: 178,
-        g: 178,
-        b: 178
-    )
-
-    static let grey200: UIColor = .init(
-        r: 204,
-        g: 204,
-        b: 204
-    )
-
-    static let grey100: UIColor = .init(
-        r: 229,
-        g: 229,
-        b: 229
-    )
-
-    static let grey50: UIColor = .init(
-        r: 250,
-        g: 250,
-        b: 250
-    )
+    static let grey900: UIColor = #colorLiteral(red: 0.1019607843, green: 0.1019607843, blue: 0.1019607843, alpha: 1)
+    static let grey800: UIColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    static let grey700: UIColor = #colorLiteral(red: 0.3019607843, green: 0.3019607843, blue: 0.3019607843, alpha: 1)
+    static let grey600: UIColor = #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+    static let grey500: UIColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+    static let grey400: UIColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+    static let grey300: UIColor = #colorLiteral(red: 0.6980392157, green: 0.6980392157, blue: 0.6980392157, alpha: 1)
+    static let grey200: UIColor = #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    static let grey100: UIColor = #colorLiteral(red: 0.8980392157, green: 0.8980392157, blue: 0.8980392157, alpha: 1)
+    static let grey50: UIColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
 }
 
 extension UIColor {
-    convenience init(r: CGFloat,
-                     g: CGFloat,
-                     b: CGFloat) {
-        self.init(
-            red: r / 255.0,
-            green: g / 255.0,
-            blue: b / 255.0,
-            alpha: 1
-        )
-    }
-
     public convenience init(light: UIColor,
                             dark: UIColor) {
         self.init {
