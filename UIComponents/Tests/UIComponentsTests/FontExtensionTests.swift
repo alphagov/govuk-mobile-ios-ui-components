18e26431b2595d74b8e715f6ffabbd562239b44d
Robin Phillips <rlphillips@deloitte.co.uk>
2024-07-29 09:50:55 +0100

    PR comments

--- UIComponents/Tests/UIComponentsTests/FontExtensionTests.swift
+++ UIComponents/Tests/UIComponentsTests/FontExtensionTests.swift
@@ -15,177 +15,177 @@ final class FontExtensionTests: XCTestCase {
     }
 
     // MARK: TITLE 1
-    func test_title1() {
+    func test_title1_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.title1
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 28)
     }
 
-    func test_title1Bold() {
-        let font = UIFont.govUK.title1
+    func test_title1Bold_hasCorrectSizeAndFont() {
+        let font = UIFont.govUK.title1Bold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 28)
     }
 
     // MARK: TITLE 2
-    func test_title2() {
+    func test_title2_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.title2
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 22)
     }
 
-    func test_title2Bold() {
-        let font = UIFont.govUK.title2
+    func test_title2Bold_hasCorrectSizeAndFont() {
+        let font = UIFont.govUK.title2Bold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 22)
     }
 
     // MARK: TITLE 3
-    func test_title3() {
+    func test_title3_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.title3
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 20)
     }
 
-    func test_title3Semibold() {
-        let font = UIFont.govUK.title3
+    func test_title3Semibold_hasCorrectSizeAndFont() {
+        let font = UIFont.govUK.title3Semibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 20)
     }
 
     // MARK: HEADLINE
-    func test_headlineSemibold() {
+    func test_headlineSemibold_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.headlineSemibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
-    func test_headlineSemiboldItalic() {
+    func test_headlineSemiboldItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.headlineSemiboldItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
     // MARK: BODY
-    func test_bodySemibold() {
+    func test_bodySemibold_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.bodySemibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
-    func test_body() {
+    func test_body_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.body
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
-    func test_bodyItalic() {
+    func test_bodyItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.bodyItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
-    func test_bodyBoldItalic() {
+    func test_bodyBoldItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.bodyBoldItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 17)
     }
 
     // MARK: CALLOUT
-    func test_calloutSemibold() {
+    func test_calloutSemibold_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.calloutSemibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 16)
     }
 
-    func test_callout() {
+    func test_callout_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.callout
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 16)
     }
 
-    func test_calloutItalic() {
+    func test_calloutItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.calloutItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 16)
     }
 
-    func test_calloutSemiboldItalic() {
+    func test_calloutSemiboldItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.calloutSemiboldItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 16)
     }
 
     // MARK: SUBHEADLINE
-    func test_subheadlineSemibold() {
+    func test_subheadlineSemibold_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.subheadlineSemibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 15)
     }
 
-    func test_subheadline() {
+    func test_subheadline_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.subheadline
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 15)
     }
 
-    func test_subheadlineItalic() {
+    func test_subheadlineItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.subheadlineItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 15)
     }
 
-    func test_subheadlineSemiboldItalic() {
+    func test_subheadlineSemiboldItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.subheadlineSemiboldItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 15)
     }
 
     // MARK: CAPTION 1
-    func test_caption1Medium() {
+    func test_caption1Medium_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption1Medium
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 12)
     }
 
-    func test_caption1() {
+    func test_caption1_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption1
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 12)
     }
 
-    func test_caption1Italic() {
+    func test_caption1Italic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption1Italic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 12)
     }
 
-    func test_caption1MediumItalic() {
+    func test_caption1MediumItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption1MediumItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 12)
     }
 
     // MARK: CAPTION 2
-    func test_caption2Semibold() {
+    func test_caption2Semibold_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption2Semibold
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 11)
     }
 
-    func test_caption2() {
+    func test_caption2_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption2
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 11)
     }
 
-    func test_caption2Italic() {
+    func test_caption2Italic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption2Italic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 11)
     }
 
-    func test_caption2SemiboldItalic() {
+    func test_caption2SemiboldItalic_hasCorrectSizeAndFont() {
         let font = UIFont.govUK.caption2SemiboldItalic
         XCTAssertEqual(font.familyName, ".AppleSystemUIFont")
         assertFontSize(font: font, size: 11)
