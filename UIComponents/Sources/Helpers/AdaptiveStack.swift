81f09088542b8ff0d97b3991cdcf4ca744bafefb
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2024-09-04 12:08:09 +0100

    Change access from internal to public

--- UIComponents/Sources/Helpers/AdaptiveStack.swift
+++ UIComponents/Sources/Helpers/AdaptiveStack.swift
@@ -1,22 +1,22 @@
 import SwiftUI
 
-struct AdaptiveStack<Content: View>: View {
+public struct AdaptiveStack<Content: View>: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass
     let horizontalAlignment: HorizontalAlignment
     let verticalAlignment: VerticalAlignment
     let content: () -> Content
-
-    init(horizontalAlignment: HorizontalAlignment = .center,
-         verticalAlignment: VerticalAlignment = . center,
-         @ViewBuilder content: @escaping () -> Content) {
+    
+    public init(horizontalAlignment: HorizontalAlignment = .center,
+                verticalAlignment: VerticalAlignment = . center,
+                @ViewBuilder content: @escaping () -> Content) {
         self.horizontalAlignment = horizontalAlignment
         self.verticalAlignment = verticalAlignment
         self.content = content
     }
-
-    var body: some View {
+    
+    public var body: some View {
         Group {
-            if  verticalSizeClass == .compact {
+            if verticalSizeClass == .compact {
                 HStack(alignment: verticalAlignment, content: content)
             } else {
                 VStack(alignment: horizontalAlignment, content: content)
