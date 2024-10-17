338fac359381c7c093f07ee5d62560b4e4e82f72
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-17 16:28:24 +0100

    Moved GOVUKColors+StrokesTests to swift testing

--- UIComponents/Tests/UIComponentsTests/Colors/GOVUKColors+StrokesTests.swift
+++ UIComponents/Tests/UIComponentsTests/Colors/GOVUKColors+StrokesTests.swift
@@ -1,19 +1,24 @@
-import XCTest
+import Foundation
+import UIKit
+import Testing
 
 @testable import UIComponents
 
-final class GOVUKColors_StrokesTests: XCTestCase {
-    func test_listDivider_returnsExpectedResult() {
+@Suite
+struct GOVUKColors_StrokesTests {
+    @Test
+    func listDivider_returnsExpectedResult() {
         let result = UIColor.govUK.strokes.listDivider
 
-        XCTAssertEqual(result.lightMode, .grey300)
-        XCTAssertEqual(result.darkMode, .grey500)
+        #expect(result.lightMode == .grey300)
+        #expect(result.darkMode == .grey500)
     }
 
-    func test_pageControlInactive_returnsExpectedResult() {
+    @Test
+    func pageControlInactive_returnsExpectedResult() {
         let result = UIColor.govUK.strokes.pageControlInactive
 
-        XCTAssertEqual(result.lightMode, .grey500)
-        XCTAssertEqual(result.darkMode, .grey300)
+        #expect(result.lightMode == .grey500)
+        #expect(result.darkMode == .grey300)
     }
 }
