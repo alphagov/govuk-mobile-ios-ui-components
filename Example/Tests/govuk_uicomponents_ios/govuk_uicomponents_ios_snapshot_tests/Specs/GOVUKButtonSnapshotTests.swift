fca61443aa6bb5a3f86bf3c22c039f319594a74a
Naveed <116517371+naveedahmad14@users.noreply.github.com>
2024-11-06 09:15:41 +0000

    Add snapshot testing to UI components (#18)
    * WIP
    
    * add snapshot images
    
    * adhere spacing to different stages,  //given, when, then

--- Example/Tests/govuk_uicomponents_ios/govuk_uicomponents_ios_snapshot_tests/Specs/GOVUKButtonSnapshotTests.swift
+++ Example/Tests/govuk_uicomponents_ios/govuk_uicomponents_ios_snapshot_tests/Specs/GOVUKButtonSnapshotTests.swift
@@ -7,7 +7,7 @@ import iOSSnapshotTestCase
 @testable import UIComponents
 
 @MainActor
-final class GOVUKButtonSnapshotTests: FBSnapshotTestCase {
+class GOVUKButtonSnapshotTests: FBSnapshotTestCase {
 
     func test_primary_rendersCorrectly() {
         let viewController = ButtonStateViewController(
@@ -49,5 +49,92 @@ final class GOVUKButtonSnapshotTests: FBSnapshotTestCase {
         window?.makeKeyAndVisible()
 
         FBSnapshotVerifyView(nav.view)
+    }
+
+    func test_primary_button_default() {
+        let button = GOVUKButton(.primary)
+        button.setTitle("primary", for: .normal)
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_primary_button_highlighted() {
+        let button = GOVUKButton(.primary)
+        button.setTitle("primary", for: .normal)
+        button.isHighlighted = true
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_primary_button_disabled() {
+        let button = GOVUKButton(.primary)
+        button.setTitle("primary", for: .normal)
+        button.isEnabled = false
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_secondary_button_default() {
+        let button = GOVUKButton(.secondary)
+        button.setTitle("secondary", for: .normal)
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_secondary_button_highlighted() {
+        let button = GOVUKButton(.secondary)
+        button.setTitle("secondary", for: .normal)
+        button.isHighlighted = true
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_secondary_button_disabled() {
+        let button = GOVUKButton(.secondary)
+        button.setTitle("secondary", for: .normal)
+        button.isEnabled = false
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_compact_button_default() {
+        let button = GOVUKButton(.compact)
+        button.setTitle("compact", for: .normal)
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_compact_button_highlighted() {
+        let button = GOVUKButton(.compact)
+        button.setTitle("compact", for: .normal)
+        button.isHighlighted = true
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func test_compact_button_disabled() {
+        let button = GOVUKButton(.compact)
+        button.setTitle("compact", for: .normal)
+        button.isEnabled = false
+
+        button.frame.size = CGSize(width: 200, height: 47)
+
+        FBSnapshotVerifyView(button)
     }
 }
