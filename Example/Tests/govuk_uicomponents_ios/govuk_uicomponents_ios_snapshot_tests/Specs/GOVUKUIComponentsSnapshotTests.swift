088051fd24056f9cf656c1adde219c3f21c57060
Naveed <116517371+naveedahmad14@users.noreply.github.com>
2025-02-18 09:57:21 +0000

    Improve UIComponents snapshot tests - add font, color, and button pages (#31)

---
+++ Example/Tests/govuk_uicomponents_ios/govuk_uicomponents_ios_snapshot_tests/Specs/GOVUKUIComponentsSnapshotTests.swift
@@ -0,0 +1,76 @@
+import Foundation
+import SwiftUI
+import XCTest
+import iOSSnapshotTestCase
+
+@testable import govuk_uicomponents_ios
+@testable import UIComponents
+
+@MainActor
+class GOVUKUIComponentsSnapshotTests: FBSnapshotTestCase {
+    override func setUp() {
+        super.setUp()
+        self.recordMode = false
+    }
+
+    func test_buttons_rendersCorrectly() {
+        let viewController = ButtonTestingViewController(
+        )
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.navigationBar.prefersLargeTitles = true
+
+        let window = UIApplication.shared.windows.first
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
+        FBSnapshotVerifyView(nav.view)
+    }
+
+    func test_font_rendersCorrectly() {
+        let viewController = FontTestingViewController()
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.navigationBar.prefersLargeTitles = true
+
+        let window = UIApplication.shared.windows.first
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
+        FBSnapshotVerifyView(nav.view)
+    }
+
+    func test_colours_fills_rendersCorrectly() {
+        let viewController = FillsColoursViewController()
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.navigationBar.prefersLargeTitles = true
+
+        let window = UIApplication.shared.windows.first
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
+        FBSnapshotVerifyView(nav.view)
+    }
+
+    func test_colours_strokes_rendersCorrectly() {
+        let viewController = StrokesColoursViewController()
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.navigationBar.prefersLargeTitles = true
+
+        let window = UIApplication.shared.windows.first
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
+        FBSnapshotVerifyView(nav.view)
+    }
+
+    func test_colours_text_rendersCorrectly() {
+        let viewController = TextColoursViewController()
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.navigationBar.prefersLargeTitles = true
+
+        let window = UIApplication.shared.windows.first
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
+        FBSnapshotVerifyView(nav.view)
+    }
+}
