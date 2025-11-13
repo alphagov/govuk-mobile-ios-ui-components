f84c752ce4b5708f3ecb2829a9f051c92c0e1439
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-11-13 10:19:27 +0000

    Added SnapshotTestCase

--- Example/Tests/govuk_uicomponents_ios/govuk_uicomponents_ios_snapshot_tests/Specs/GOVUKUIComponentsSnapshotTests.swift
+++ Example/Tests/govuk_uicomponents_ios/govuk_uicomponents_ios_snapshot_tests/Specs/GOVUKUIComponentsSnapshotTests.swift
@@ -7,70 +7,34 @@ import iOSSnapshotTestCase
 @testable import UIComponents
 
 @MainActor
-class GOVUKUIComponentsSnapshotTests: FBSnapshotTestCase {
+class GOVUKUIComponentsSnapshotTests: SnapshotTestCase {
     override func setUp() {
         super.setUp()
         self.recordMode = false
     }
 
     func test_buttons_rendersCorrectly() {
-        let viewController = ButtonTestingViewController(
-        )
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.navigationBar.prefersLargeTitles = false
-
-        let window = UIApplication.shared.windows.first
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-
-        FBSnapshotVerifyView(nav.view)
+        let viewController = ButtonTestingViewController()
+        VerifySnapshot(viewController)
     }
 
     func test_font_rendersCorrectly() {
         let viewController = FontTestingViewController()
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.navigationBar.prefersLargeTitles = true
-
-        let window = UIApplication.shared.windows.first
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-
-        FBSnapshotVerifyView(nav.view)
+        VerifySnapshot(viewController)
     }
 
     func test_colours_fills_rendersCorrectly() {
         let viewController = FillsColoursViewController()
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.navigationBar.prefersLargeTitles = true
-
-        let window = UIApplication.shared.windows.first
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-
-        FBSnapshotVerifyView(nav.view)
+        VerifySnapshot(viewController.view)
     }
 
     func test_colours_strokes_rendersCorrectly() {
         let viewController = StrokesColoursViewController()
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.navigationBar.prefersLargeTitles = true
-
-        let window = UIApplication.shared.windows.first
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-
-        FBSnapshotVerifyView(nav.view)
+        VerifySnapshot(viewController)
     }
 
     func test_colours_text_rendersCorrectly() {
         let viewController = TextColoursViewController()
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.navigationBar.prefersLargeTitles = true
-
-        let window = UIApplication.shared.windows.first
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-
-        FBSnapshotVerifyView(nav.view)
+        VerifySnapshot(viewController)
     }
 }
